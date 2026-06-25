fd2a88974c8daa35266dfe5d80e58815f88eb5aa
Aqrapure <royf.101@gmail.com>
2026-06-25 17:35:07 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -176,9 +176,10 @@ export default function Home() {
       <div style={galleryStyle}>
   <img src="/stock-twobed-house.jpg" style={imageStyle} />
 <img src="/glass-roof1-after.jpg" style={imageStyle} />
-<img src="/gutter-before .jpg" style={imageStyle} />
+<img src="/gutter-before.jpg" style={imageStyle} />
 <img src="/gutter-after.jpg" style={imageStyle} />
-<img src="/pressure-wash.jpg" style={imageStyle} />
+<img src="/driveway-before-after.jpg" style={imageStyle} />
+<img src="/Pattio Pressure Wash.jpg" style={imageStyle} />
 <img src="/commercial-one.jpg" style={imageStyle} />
 </div>
       </section>
