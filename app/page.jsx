62dd2df83060ee090774c13dc717d94a1db51a66
Aqrapure <royf.101@gmail.com>
2026-07-08 22:37:32 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -204,7 +204,7 @@ export default function Home() {
      alt="Conservatory roof cleaning Northampton"
      style={imageStyle} />
 
-<img src="/gutter-before .jpg" alt="Blocked gutter before cleaning" style={imageStyle} />
+<img src="/new-gutter-before.jpg" alt="Blocked gutter before clearing" style={imageStyle} />
 
 <img src="/gutterafter-clean.jpg" alt="Clean gutter after professional gutter cleaning" style={imageStyle} />
 
