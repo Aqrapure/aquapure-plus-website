d66f5bdcec6aa7431e645ea2ec46bb8a85404b70
Aqrapure <royf.101@gmail.com>
2026-07-30 10:13:56 +0100

    Update ServicePage.jsx

--- app/components/ServicePage.jsx
+++ app/components/ServicePage.jsx
@@ -112,7 +112,7 @@ Additional information:`;
           }}
         >
           <a
-            href="tel:+447448863669"
+            href="tel:+447860150637"
             style={{
               ...contactButtonStyle,
               background: "#0b5fa5",
@@ -122,7 +122,7 @@ Additional information:`;
           </a>
 
           <a
-            href="https://wa.me/447448863669"
+            href="https://wa.me/447860150637"
             target="_blank"
             rel="noopener noreferrer"
             style={{
